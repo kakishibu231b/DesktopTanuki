4c3e16bc158becda0823dfdf368a6929cfa33bff
柿渋 <kakishibu231b@gmail.com>
2022-01-31 17:47:06 +0900

    プロジェクト ファイルを追加します。

---
+++ DesktopTanuki/TanukiView.Designer.cs
@@ -0,0 +1,105 @@
+﻿
+namespace DesktopTanuki
+{
+    partial class TanukiBody
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuItemQuit = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripMenuItemVersion = new System.Windows.Forms.ToolStripMenuItem();
+            this.tanukiByeTimer = new System.Windows.Forms.Timer(this.components);
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItemQuit,
+            this.toolStripMenuItem1,
+            this.toolStripMenuItemVersion});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(166, 54);
+            // 
+            // toolStripMenuItemQuit
+            // 
+            this.toolStripMenuItemQuit.Name = "toolStripMenuItemQuit";
+            this.toolStripMenuItemQuit.Size = new System.Drawing.Size(165, 22);
+            this.toolStripMenuItemQuit.Text = "バイバイ(^^)/~~~";
+            this.toolStripMenuItemQuit.Click += new System.EventHandler(this.toolStripMenuItemQuit_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(162, 6);
+            // 
+            // toolStripMenuItemVersion
+            // 
+            this.toolStripMenuItemVersion.Name = "toolStripMenuItemVersion";
+            this.toolStripMenuItemVersion.Size = new System.Drawing.Size(165, 22);
+            this.toolStripMenuItemVersion.Text = "バージョン情報";
+            // 
+            // tanukiByeTimer
+            // 
+            this.tanukiByeTimer.Tick += new System.EventHandler(this.tanukiByeTimer_Tick);
+            // 
+            // TanukiBody
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(500, 500);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "TanukiBody";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Tanuki";
+            this.TransparencyKey = System.Drawing.SystemColors.Control;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.TanukiView_FormClosing);
+            this.Load += new System.EventHandler(this.Tanuki_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.TanukiBody_Paint);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.TanukiView_MouseClick);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemVersion;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemQuit;
+        private System.Windows.Forms.Timer tanukiByeTimer;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+    }
+}
+
---
+++ DesktopTanuki/TanukiView.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DesktopTanuki
+{
+    public partial class TanukiBody : Form
+    {
+        bool bolBye;
+
+        /// <summary>
+        /// たぬき本体
+        /// </summary>
+        public TanukiBody()
+        {
+            InitializeComponent();
+
+            bolBye = false;
+        }
+
+        /// <summary>
+        /// たぬき召喚
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Tanuki_Load(object sender, EventArgs e)
+        {
+            //------------------------------
+            // 初期表示位置の決定
+            //------------------------------
+            Bitmap bmp_skin = global::DesktopTanuki.Properties.Resources.tanuki_001;
+
+
+            FrameDimension frameDimension = new FrameDimension(bmp_skin.FrameDimensionsList[0]);
+            int int_frame_count = bmp_skin.GetFrameCount(frameDimension);
+
+            // 全フレームをチェックしてY座標最大値を取得する。
+            int int_bottom = -1;
+            for (int int_frame_no = 0; int_frame_no < int_frame_count; ++int_frame_no)
+            {
+                bmp_skin.SelectActiveFrame(frameDimension, int_frame_no);
+
+                for (int int_y = Height - 1; int_y >= 0; --int_y)
+                {
+                    bool bol_found = false;
+                    for (int int_x = 0; int_x < Width; ++int_x)
+                    {
+                        Color color = bmp_skin.GetPixel(int_x, int_y);
+                        if (color.A != 0)
+                        {
+                            bol_found = true;
+                            if (int_bottom < int_y)
+                            {
+                                int_bottom = int_y;
+                            }
+                            break;
+                        }
+                    }
+                    if (bol_found)
+                    {
+                        break;
+                    }
+               }
+            }
+
+            Height = bmp_skin.Height;
+            Width = bmp_skin.Width;
+            BackgroundImage = bmp_skin;
+
+
+            // タスクバーを除くデスクトップ作業領域サイズ
+            int screen_width = Screen.PrimaryScreen.WorkingArea.Width;
+            int screen_height = Screen.PrimaryScreen.WorkingArea.Height;
+            // たぬきサイズ
+            int tanuki_width = Width;
+            int tanuki_height = Height;
+
+
+
+            // 初期表示位置設定
+            Location = new Point(screen_width - tanuki_width, screen_height - int_bottom);
+
+            // たぬき動作許可
+            ImageAnimator.Animate(BackgroundImage, new EventHandler(Image_FrameChanged));
+        }
+
+        /// <summary>
+        /// たぬき動作
+        /// </summary>
+        /// <param name="o"></param>
+        /// <param name="e"></param>
+        private void Image_FrameChanged(object o, EventArgs e)
+        {
+            Invalidate();
+        }
+
+        /// <summary>
+        /// たぬき動作
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void TanukiBody_Paint(object sender, PaintEventArgs e)
+        {
+            ImageAnimator.UpdateFrames(BackgroundImage);
+        }
+
+        private void TanukiView_MouseClick(object sender, MouseEventArgs e)
+        {
+           
+            if (e.Button == MouseButtons.Left)
+            {
+            }
+            if (e.Button == MouseButtons.Right)
+            {
+                contextMenuStrip1.Show(Cursor.Position, ToolStripDropDownDirection.AboveLeft);
+            }
+        }
+
+        /// <summary>
+        /// たぬきバイバイ
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void toolStripMenuItemQuit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void tanukiByeTimer_Tick(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        /// <summary>
+        /// たぬきバイバイ
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void TanukiView_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if  (!bolBye)
+            {
+                tanukiByeTimer.Interval = 1000;
+                tanukiByeTimer.Enabled = true;
+
+                BackgroundImage.Dispose();
+                BackgroundImage = global::DesktopTanuki.Properties.Resources.tanuki_002;
+                ImageAnimator.Animate(BackgroundImage, new EventHandler(Image_FrameChanged));
+
+                e.Cancel = true;
+
+                bolBye = true;
+            }
+        }
+    }
+}
